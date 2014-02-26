c2c6e5a6c8c65dcd4c1431806377ead2a8a95d04
unknown <tvoice@tm100.TSAO-MCKOWN.COM>
2014-02-26 16:28:22 -0500

    cleaned up newsletter for gist library

---
+++ js.js
@@ -0,0 +1,56 @@
+var all = document.getElementsByTagName('*');
+remove_style(all);
+
+function remove_style(all) {
+  var i = all.length;
+  var j, is_hidden;
+
+  // Presentational attributes.
+  var attr = [
+    'align',
+    'background',
+    'bgcolor',
+    'border',
+    'cellpadding',
+    'cellspacing',
+    'color',
+    'face',
+    'height',
+    'hspace',
+    'marginheight',
+    'marginwidth',
+    'noshade',
+    'nowrap',
+    'valign',
+    'vspace',
+    'width',
+    'vlink',
+    'alink',
+    'text',
+    'link',
+    'frame',
+    'frameborder',
+    'clear',
+    'scrolling',
+    'style'
+  ];
+
+  var attr_len = attr.length;
+
+  while (i--) {
+    is_hidden = (all[i].style.display === 'none');
+
+    j = attr_len;
+
+    while (j--) {
+      all[i].removeAttribute(attr[j]);
+    }
+
+    // Re-hide display:none elements,
+    // so they can be toggled via JS.
+    if (is_hidden) {
+      all[i].style.display = 'none';
+      is_hidden = false;
+    }
+  }
+}
